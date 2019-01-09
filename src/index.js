cdd943247ae7ee7270ba1ffc3f034cfe04944a3f
LasaleFamine <info@godev.space>
2019-01-09 19:47:19 +0100

    fix: re-adding redirect to https

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {resolve, join} = require('path');
 const {createServer} = require('https');
 const express = require('express');
 const fallback = require('express-history-api-fallback');
+const {redirectToHTTPS} = require('express-http-to-https');
 const certificate = require('devcert-san');
 
 const pupperender = require('pupperender');
@@ -20,12 +21,16 @@ module.exports = async (folder, options) => {
 	const HOST = opt.h || opt.host || getStHost();
 	const FALLINDEX = opt.f || opt.fallback || 'index.html';
 	const DEBUG = opt.d || opt.debug || false;
+	const LOCALHTTPS = opt.s || opt.https || false;
 	const CACHE = opt.c || opt.cache || false;
 	const CACHE_TTL = opt.cacheTTL || 3600;
 	const SSL = opt.ssl || false;
 	const IS_DEV = process.env.NODE_ENV !== 'production';
 
 	const app = express();
+
+	// Don't redirect if the hostname is `localhost` or `127.0.0.1`
+	app.use(redirectToHTTPS(LOCALHTTPS ? [] : [/localhost|127.0.0.1/]));
 
 	app.use(loggerMiddleware(DEBUG));
 	app.use(pupperender.makeMiddleware({debug: DEBUG, useCache: CACHE, cacheTTL: CACHE_TTL}));
